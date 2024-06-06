0aa2a24c1d17f4865dc7f4660ffc3fc2c17a8484
Oscar Fernandez <oscar.leif@gmail.com>
2024-06-06 01:01:49 -0500

    We fix the issue with the camera corners to world space.
    More improve...basically we are writing the game core.
    really took us time to setup the ground and player movement.

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -4,7 +4,7 @@ export(NodePath) var sprite_path
 export(NodePath) var Player_path
 
 export(float) var cameraOffsetX = 0
-export(float) var playerSpeed = 2
+export(float) var playerSpeed = 1
 export(int) var num_sprites_each_side = 4
 
 var sprite_copies = []
@@ -12,14 +12,17 @@ onready var sprite3D: Sprite3D = get_node("Land/Land0")
 onready var NodeLand:Node = get_node("Land")
 onready var camera:Camera = get_node("Camera")
 
+var sprite3d_width:float
 var Player: RigidBody
 
 
 func _ready():
 	var sprite_width = sprite3D.texture.get_size().x * sprite3D.pixel_size #return texture to world size
+	sprite3d_width = sprite_width
 	var originalSpritePos = sprite3D.global_position
 	print("Original pos: " + str(originalSpritePos))
 	print("Sprite Width : " + str(sprite_width))
+	sprite_copies.append(sprite3D)
 	for i in range(num_sprites_each_side):
 		#create left
 		var left_sprite = sprite3D.duplicate()
@@ -30,11 +33,11 @@ func _ready():
 		sprite_copies.append(left_sprite)
 		
 #		#create right
-		var right_sprite = sprite3D.duplicate()
-		var spawnPosRight = Vector3(sprite3D.global_position.x+(i+1)* sprite_width, sprite3D.position.y,0 )
-		left_sprite.global_position = spawnPosRight
-		NodeLand.add_child(right_sprite)
-		sprite_copies.append(right_sprite)		
+#		var right_sprite = sprite3D.duplicate()
+#		var spawnPosRight = Vector3(sprite3D.global_position.x+(i+1)* sprite_width, sprite3D.position.y,0 )
+#		left_sprite.global_position = spawnPosRight
+#		NodeLand.add_child(right_sprite)
+#		sprite_copies.append(right_sprite)		
 	Player = get_node(Player_path)
 
 func _process(delta):
@@ -42,6 +45,7 @@ func _process(delta):
 	Player.translation.x += delta * playerSpeed
 	
 	_cameraController()
+	_groundController()
 	pass
 	
 func _cameraController():
@@ -53,17 +57,49 @@ func _cameraController():
 
 #	we just swap the far left to next far right when a sprite is outside the view 
 func _groundController():
-	var cameraDepth = abs(camera.translation.z)
-	var viewport_size = Vector2(get_viewport().size.x,  get_viewport().size.y)
-	var top_left = camera.project_position(Vector2(0, 0) , cameraDepth)
-	var top_right = camera.project_position(Vector2(viewport_size.x, 0), cameraDepth)
-	var bottom_left = camera.project_position(Vector2(0, viewport_size.y), cameraDepth)
-	var bottom_right = camera.project_position(Vector2(viewport_size.x, viewport_size.y),cameraDepth)
+	# Get camera orthographic size and viewport size
+	var orth_size = camera.size
+	var viewport_size = get_viewport().size
+	var aspect_ratio = viewport_size.x / viewport_size.y
 	
+	# Calculate the half extents of the orthographic projection
+	var half_height = orth_size
+	var half_width = orth_size * aspect_ratio
+	
+	# Get the camera's global position
+	var camera_pos = camera.global_transform.origin
+	
+	# Calculate the corners in world space
+	var top_left = camera_pos + Vector3(-half_width, half_height, 0)
+	var top_right = camera_pos + Vector3(half_width, half_height, 0)
+	var bottom_left = camera_pos + Vector3(-half_width/2, -half_height, 0)
+	var bottom_right = camera_pos + Vector3(half_width, -half_height, 0)
+	
+#	print("Top Left: ", top_left)
+#	print("Top Right: ", top_right)
+#	print("Bottom Left: ", bottom_left)
+#	print("Bottom Right: ", bottom_right)
+	
+	# Example usage for repositioning sprites
 	for sprite3D in sprite_copies:
+		var spritePos = sprite3D.translation
 		var aabb = sprite3D.get_aabb()
 		var sprite_min = aabb.position
+		var sprite_max = spritePos+ aabb.position + aabb.size
 		
-		
-		pass
-		
+		# If the sprite is out of the left boundary, move it to the right
+		if sprite_max.x < bottom_left.x:
+			var rightmost_sprite = sprite_copies[0]
+			for s in sprite_copies:
+				if s.translation.x >= rightmost_sprite.translation.x:
+					rightmost_sprite = s	
+			
+			# Calculate new position for the sprite that moved out of view
+			var new_position = rightmost_sprite.translation
+			new_position.x += sprite3d_width
+			sprite3D.translation = new_position
+			break
+
+	# Optional: Log to check positions
+#	for sprite3D in sprite_copies:
+#		print("Sprite position: " + str(sprite3D.translation))
